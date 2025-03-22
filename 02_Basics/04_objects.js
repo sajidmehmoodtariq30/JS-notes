9e763734fbf709c76dd97d204a3a4c83f89f8224
Sajid M Tariq <sajidmehmoodtariq5@gmail.com>
2025-03-23 02:50:10 +0500

    destructuring and json

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -15,5 +15,43 @@ const obj2 = {3:'c',4:'d'}
 
 
 const obj3 = {...obj1, ... obj2}
-console.log(obj3);
+// console.log(obj3);
 
+const dummydata = [
+    {
+        name: "Rehan",
+        age: 19
+    },
+    {
+        name: "Haadi",
+        age: 18
+    },
+    {
+        name: "Mooez",
+        age: 20
+    }
+]
+
+// console.log(dummydata[0].name);
+
+
+// console.log(Object.keys(usrProfile));
+// console.log(Object.values(usrProfile));
+
+// console.log(usrProfile.hasOwnProperty('id'));
+
+const course = {
+    name: "PF",
+    price: 9999,
+    instructor: "Aizaz"
+}
+
+const {name : nm, price, instructor : ins} = course
+console.log(nm, price, ins);
+
+const navbar = ({course}) => {
+    console.log(name);
+    
+}
+
+{/* <Navbar course = {course} /> */}
